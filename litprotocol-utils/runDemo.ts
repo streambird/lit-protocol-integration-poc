a8386b0299f312200a4b4dcef7ea98bac739979b
Timothy Chung <timchunght@gmail.com>
2022-07-18 12:57:09 -0400

    add runDemo

---
+++ litprotocol-utils/runDemo.ts
@@ -0,0 +1,5 @@
+import { litProtocolSignTypedDataV1 } from './signTypedTx'
+
+// TODO(JL): add example typedV1 message here for sandbox.
+const message = {}
+await litProtocolSignTypedDataV1(message);
