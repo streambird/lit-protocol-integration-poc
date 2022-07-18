c443f5cfacd7e3b40637f5ffe74118f35adb9a13
Timothy Chung <timchunght@gmail.com>
2022-07-18 19:08:11 -0400

    update README

--- litprotocol-utils/src/eth-signing-methods.ts
+++ litprotocol-utils/src/eth-signing-methods.ts
@@ -1,36 +1,46 @@
-import { legacyToBuffer } from './buffer-utils';
-import { soliditySHA3 } from 'ethereumjs-abi';
-import { bufferToHex } from 'ethereumjs-util';
+import { legacyToBuffer } from "./buffer-utils";
+import { soliditySHA3 } from "ethereumjs-abi";
+import { bufferToHex } from "ethereumjs-util";
 
 export function typedSignatureHash(typedData: any) {
   const hashBuffer = _typedSignatureHash(typedData);
   return bufferToHex(hashBuffer);
 }
 
+// This code is taken from 
+// https://github.com/MetaMask/eth-sig-util/blob/cde38f7aa3f8e8d0a7ee75730f363325b8e1924c/src/sign-typed-data.ts#L435
 export function _typedSignatureHash(typedData: any) {
-  const error = new Error('Expect argument to be non-empty array')
-  if (typeof typedData !== 'object' ||
-      !('length' in typedData) ||
-      !typedData.length) {
-      throw error
+  const error = new Error("Expect argument to be non-empty array");
+  if (
+    typeof typedData !== "object" ||
+    !("length" in typedData) ||
+    !typedData.length
+  ) {
+    throw error;
   }
+
   const data = typedData.map(function (e: any) {
-      if (e.type !== 'bytes') {
-          return e.value
-      }
-      return legacyToBuffer(e.value)
-  })
+    if (e.type !== "bytes") {
+      return e.value;
+    }
+
+    return legacyToBuffer(e.value);
+  });
   const types = typedData.map(function (e: any) {
-      return e.type
-  })
+    return e.type;
+  });
   const schema = typedData.map(function (e: any) {
-      if (!e.name) {
-          throw error
-      }
-      return `${e.type} ${e.name}`
-  })
-  return soliditySHA3(['bytes32', 'bytes32'], [
-      soliditySHA3(new Array(typedData.length).fill('string'), schema),
+    if (!e.name) {
+      throw error;
+    }
+    return `${e.type} ${e.name}`;
+  });
+
+  return soliditySHA3(
+    ["bytes32", "bytes32"],
+    [
+      soliditySHA3(new Array(typedData.length).fill("string"), schema),
       soliditySHA3(types, data),
-  ])
+    ]
+  );
 }
--- litprotocol-utils/src/signTypedTx.ts
+++ litprotocol-utils/src/signTypedTx.ts
@@ -1,4 +1,5 @@
-import { SignTypedDataVersion, typedSignatureHash, recoverTypedSignature } from '@metamask/eth-sig-util'
+import { SignTypedDataVersion, recoverTypedSignature } from '@metamask/eth-sig-util'
+import { _typedSignatureHash } from './eth-signing-methods';
 import { LitProtocolUtils } from './lit-protocol-utils'
 import { recoverAddress, recoverPublicKey } from 'ethers/lib/utils'
 
@@ -7,11 +8,11 @@ const litProtocol = new LitProtocolUtils();
 export const litProtocolSignTypedDataV1 = async (message: any) => {
     try {
         await litProtocol.connected
-        const signatureHash = typedSignatureHash(message)
-        console.log('signatureHash:', signatureHash)
-        const keccak256Hash = litProtocol.getKeccak256Hash(signatureHash)
-        console.log('keccak256Hash:',keccak256Hash)
-        const signatures = await litProtocol.executeJS(keccak256Hash)
+        // According to metamask util here https://github.com/MetaMask/eth-sig-util/blob/cde38f7aa3f8e8d0a7ee75730f363325b8e1924c/src/sign-typed-data.ts#L516
+        // the signatureHash is passed to:
+        // ecsign -> ecdsaSign(msgHash, privateKey) https://github.com/ethereumjs/ethereumjs-util/blob/master/src/signature.ts#L26
+        const msgHash = _typedSignatureHash(message)
+        const signatures = await litProtocol.executeJS(msgHash)
         const { encodedSignature, messageHash } = litProtocol.encodeSignature(signatures.data)
         const recoveredPubkey = recoverPublicKey(messageHash, encodedSignature)
         const recoveredAddress = recoverAddress(messageHash, encodedSignature)
